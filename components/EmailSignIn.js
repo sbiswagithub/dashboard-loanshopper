8b3fc1648b28ddb3229131d0bd71181d3d8f39a4
loanshopper <63908023+loanshopper@users.noreply.github.com>
2023-08-03 09:16:01 +1000

    Update EmailSignIn.js

--- components/EmailSignIn.js
+++ components/EmailSignIn.js
@@ -1,8 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { View, } from 'react-native';
 import { connect } from 'react-redux';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Card } from 'react-native-elements';
+import { Switch, Button, Paragraph, Dialog, Portal, } from 'react-native-paper';
+import Moment from 'moment';
 
 import getStyleSheet from '../styles/styles';  
 import { LOGO_DARK_BLUE, LOGO_BRIGHT_BLUE, WHITE, TRANSPARENT } from "../constants/colors";
@@ -12,15 +14,32 @@ import LoanShopperLogin from './common/LoanShopperLogin';
 import ResetPassword from './ResetPassword';
 import {  } from "../constants/banners";
 
-function LoginButton (props) {
-	return (
-		<>{props => (
-			<Icon.Button name={'email-lock'} onPress={props.onPress}
-						backgroundColor={ LOGO_DARK_BLUE}  color={WHITE} size={40} borderRadius={10} 
-						iconStyle={{flexGrow:1, flexDirection:"row",  writingDirection:"rtl" }} >Login</Icon.Button>
-		)}
-		</>
-	)
+function Countdown ( props)  {
+	const [visible,setVisible] = useState(props.show)
+		const styles = getStyleSheet();
+
+ return (
+	 <>
+		<Icon.Button name={'email-plus'} size={40} borderRadius={10} 
+			backgroundColor={WHITE}  color={LOGO_DARK_BLUE} 
+			iconStyle={{flexGrow:1, flexDirection:"row",  writingDirection:"rtl" }}
+			onPress={() => {
+				if (Moment('12/08/2023','DD/MM/YYYY').isBefore(Moment(new Date())))
+					RootNavigation.navigate('EmailRegistration')
+				else
+					setVisible(true)
+			}} >Sign up</Icon.Button>	 
+		<Portal>
+			<Dialog visible={visible} style={{maxWidth:'50%', alignSelf:"center"}} >
+			<Dialog.Title>Coming soon</Dialog.Title>
+			<Dialog.Content><Paragraph style={styles.textLargeBoldLogoDarkBlue}>{'Launch date 12th August 2023'}</Paragraph></Dialog.Content>
+			<Dialog.Actions>
+				<Button onPress={() => {setVisible(false) }}>{'Close'}</Button>
+			</Dialog.Actions>
+			</Dialog>
+		</Portal>
+	</>
+ )
 }
 
 class EmailSignIn extends Component {
@@ -31,6 +50,7 @@ class EmailSignIn extends Component {
     render () {
 		const styles = getStyleSheet();
 		//console.log(this.props)
+		console.log(Moment('12/08/2023','DD/MM/YYYY').isBefore(Moment(new Date())))
     	return (
 			<Card containerStyle={[styles.brokerCard, {backgroundColor:LOGO_BRIGHT_BLUE}]} 
 				title={'Title'} titleStyle={styles.cardTitle} >
@@ -45,12 +65,13 @@ class EmailSignIn extends Component {
 				</View>
 				<View style={styles.space} />
 				<View style={styles.brokerCardPanel}>
+					{(Moment('12/08/2023','DD/MM/YYYY').isAfter(Moment(new Date()))) ? <Countdown /> :
 					<Icon.Button name={'email-plus'} size={40} borderRadius={10} 
 						backgroundColor={WHITE}  color={LOGO_DARK_BLUE} 
 						iconStyle={{flexGrow:1, flexDirection:"row",  writingDirection:"rtl" }}
 						onPress={() => {
 							RootNavigation.navigate('EmailRegistration')
-						}} >Sign up</Icon.Button>
+						}} >Sign up</Icon.Button>}
 				</View>
 				<View style={styles.space} />
 				<View style={styles.brokerCardPanel}>
