d8f1f4e6bc927818eb405aff54b5a4e850c98e69
loanshopper <63908023+loanshopper@users.noreply.github.com>
2025-03-06 12:44:20 +1000

    Added appstores handler

--- Root.js
+++ Root.js
@@ -7,6 +7,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { navigationRef } from "./actions/RootNavigation";
 
+import AppStores from "./components/appstores";
 import Landing from "./components/Landing";
 import Menu from "./components/menu/Menu";
 import VerifyOtp from "./components/otp/VerifyOtp";
@@ -20,12 +21,14 @@ import { webAuthenticatedBorrower, onRedirect } from './actions/Auth'
 
 function Root() {
   const dispatch = useDispatch();
-  const search = useLocation().search;
+  const location = useLocation()
+  const search = location.search;
   const Stack = createStackNavigator();
   const redirectData = Object.fromEntries(
     new URLSearchParams(search)
   )
-  //console.log(redirectData)
+  console.log(location)
+  console.log(redirectData)
 
   if (redirectData?.accessCode) {
     //console.log('Preauthenticated')
@@ -40,11 +43,13 @@ function Root() {
   return (
       <NavigationContainer ref={navigationRef}>
         <Stack.Navigator initialRouteName={
+          location?.pathname === '/appstores' ? 'AppStores' :
           !redirectData?.appEntryMode ? 'Landing' : 
           redirectData?.appEntryMode && redirectData.appEntryMode.toUpperCase() === "EMAIL_REGISTRATION_START" ? 'VerifyOtp' :
           redirectData?.appEntryMode && redirectData.appEntryMode.toUpperCase() === "REGISTRATION_IN_PROGRESS" ? 'UserRegistration' :
           'Menu'} >
           <>
+            <Stack.Screen name="AppStores" component={AppStores} options={{ headerShown: false, gestureEnabled: false, }} />
             <Stack.Screen name="Landing" component={Landing} options={{ headerShown: false, gestureEnabled: false, }} />
             <Stack.Screen name="EmailRegistration" component={EmailRegistration} options={{ headerShown: false, gestureEnabled: false, }}  />
             <Stack.Screen name="EmailRegistration2" component={EmailRegistration2} options={{ headerShown: false, gestureEnabled: false, }}  />
